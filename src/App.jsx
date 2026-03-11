03f431170e4710caa11a7e2340e9f4866bc29eff
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-03-12 00:00:20 +0530

    implement Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Card(props){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{border:"2px solid black", padding:"2px"}}>
+      <img src="https://yufta.com/cdn/shop/articles/blue_dupatta_set.jpg?v=1647416059" height="200px" width="200px" />
+      <div style={{textAlign:"center"}}>
+        <h2>{props.category}</h2>
+        <h1>{props.offer} OFF</h1>
+        <h2>Shop Now</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const arr = [
+  {category:"Ethnic Wear", offer:"50-80%"},
+  {category:"Casual Wear", offer:"40-80%"},
+  {category:"Men's Activewear", offer:"30-70%"},
+  {category:"Women's Activewear", offer:"30-70%"},
+  {category:"Western Wear", offer:"40-80%"},
+  {category:"Sportswear", offer:"30-80%"}
+]
+
+function App() {
+  return (
+    // header
+    // body
+    <div style={{display:"flex", gap:"10px", flexWrap:"wrap"}}>
+      {arr.map((value, index) => <Card key={index} category={value.category} offer={value.offer} />)}
+    </div>
+    // footer
+  );
+}
+
+export default App;
